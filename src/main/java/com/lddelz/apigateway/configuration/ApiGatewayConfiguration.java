1a637be90d69bdb74886fed0b144f84f63e19c25
lukasz.danelczyk <lukasz.danelczyk.sp@lhsystems.com>
2022-05-25 18:26:33 +0200

    Update spring cloud version and clean up

--- src/main/java/com/lddelz/apigateway/configuration/ApiGatewayConfiguration.java
+++ src/main/java/com/lddelz/apigateway/configuration/ApiGatewayConfiguration.java
@@ -21,13 +21,8 @@ public class ApiGatewayConfiguration {
                         .uri("lb://currency-exchange"))
                 .route(p -> p.path("/currency-conversion/**")
                         .uri("lb://currency-conversion"))
-                .route(p -> p.path("/currency-conversion-feign/**")
-                        .uri("lb://currency-conversion"))
-                .route(p -> p.path("/currency-conversion-new/**")
-                        .filters(f -> f.rewritePath(
-                                "/currency-conversion-new/(?<segment>.*)",
-                                "/currency-conversion-feign/${segment}"))
-                        .uri("lb://currency-conversion"))
+                .route(p -> p.path("/limits/**")
+                        .uri("lb://limits-service"))
                 .build();
     }
 
--- src/main/java/com/lddelz/apigateway/filter/LoggingFilter.java
+++ src/main/java/com/lddelz/apigateway/filter/LoggingFilter.java
@@ -15,7 +15,7 @@ public class LoggingFilter implements GlobalFilter {
 
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
-        logger.info("Path of the request received -> {}", exchange.getRequest().getPath());
+        logger.info("Path of the request has been received -> {}", exchange.getRequest().getPath());
         return chain.filter(exchange);
     }
 
